51eaf8e57de903f5d11641516b7b9af3452c010a
Nucoco <yuuta11511997@gmail.com>
2024-04-20 13:53:31 +0000

    initial settings

---
+++ packages/contract/scripts/deploy.js
@@ -0,0 +1,21 @@
+async function main() {
+  // コントラクトを扱うために必要なファイルが `artifacts` ディレクトリの直下に生成されます。
+  // getContractFactory関数は、デプロイをサポートするライブラリのアドレスとMyEpicNFTコントラクトの連携を行っています。
+  // Hardhat Runtime Environment（HRE）は、Hardhatが用意したすべての機能を含むオブジェクト（＝コードの束）です。
+  // hardhatで始まるターミナルコマンドを実行するたびに、HREにアクセスしているので、hreをdeploy.jsにインポートする必要はありません。
+  const nftContractFactory = await hre.ethers.getContractFactory("MyEpicNFT");
+
+  // HardhatがローカルのEthereumネットワークを、コントラクトのためだけに作成します。
+  // そして、スクリプトの実行が完了した後、そのローカル・ネットワークを破棄します。
+  // つまり、コントラクトを実行するたびに、毎回ローカルサーバーを更新するかのようにブロックチェーンが新しくなります。
+  const nftContract = await nftContractFactory.deploy();
+
+  // コントラクトが Mint され、ローカルのブロックチェーンにデプロイされるまで待ちます。
+  await nftContract.deployed();
+  console.log("Contract deployed to:", nftContract.address);
+};
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
