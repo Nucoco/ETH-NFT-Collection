451a8cc5c68305499bd99be01814135b81d4ae23
Nucoco <yuuta11511997@gmail.com>
2024-04-21 10:22:07 +0000

    mint機能の追加

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,3 +1,5 @@
+import { ethers } from "ethers";
+import myEpicNft from "./utils/MyEpicNFT.json";
 import React, { useEffect, useState } from "react";
 import "./styles/App.css";
 import twitterLogo from "./assets/twitter-logo.svg";
@@ -48,6 +50,35 @@ const App = () => {
     }
   };
 
+  const askContractToMintNft = async () => {
+    const CONTRACT_ADDRESS = "0xC71fffE3a93B1F76f88d87504c1A97E0b4896Ad2";
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        // metamaskのイーサリアムノードへの接続
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        // ユーザーアドレス
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEpicNft.abi,
+          // providerを渡すとread機能のみ使える
+          signer
+        );
+        console.log("Going to pop wallet now to pay gas...");
+        let nftTxn = await connectedContract.makeAnEpicNFT();
+        console.log("Mining...please wait.");
+        await nftTxn.wait();
+
+        console.log(`Mined, see transaction: https://sepolia.etherscan.io/tx/${nftTxn.hash}`);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const renderNotConnectedContainer = () => (
     <button
       onClick={connectWallet}
@@ -70,7 +101,7 @@ const App = () => {
           {currentAccount === "" ? (
             renderNotConnectedContainer()
           ) : (
-            <button onClick={null} className="cta-button connect-wallet-button">
+            <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
               Mint NFT
             </button>
           )}
